1181310d75bad2c47c90b2334450b691f2ef803e
Aly Baez <alybaez@Alys-MBP.lan>
2022-06-10 09:35:28 -0700

    successfully displays fb data onto UI

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -4,84 +4,19 @@ import styled from "styled-components";
 
 import EmployeeCard from "./EmployeeCard";
 
-function EmployeeList() {
-  const data = [
-    {
-      name: "Jim Jam",
-      birthday: "09/20/1999",
-      department: "Technology",
-      phone: "909-999-9999",
-      email: "jim@gmail.com",
-      image:
-        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
-    },
-    {
-      name: "Cary Smith",
-      birthday: "09/20/1999",
-      department: "Technology",
-      phone: "909-999-9999",
-      email: "jim@gmail.com",
-      image:
-        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
-    },
-    {
-      name: "Jim Jam",
-      birthday: "09/20/1999",
-      department: "Technology",
-      phone: "909-999-9999",
-      email: "jim@gmail.com",
-      image:
-        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
-    },
-    {
-      name: "Cary Smith",
-      birthday: "09/20/1999",
-      department: "Technology",
-      phone: "909-999-9999",
-      email: "jim@gmail.com",
-      image:
-        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
-    },
-    {
-      name: "Jim Jam",
-      birthday: "09/20/1999",
-      department: "Technology",
-      phone: "909-999-9999",
-      email: "jim@gmail.com",
-      image:
-        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
-    },
-    {
-      name: "Jim Jam",
-      birthday: "09/20/1999",
-      department: "Technology",
-      phone: "909-999-9999",
-      email: "jim@gmail.com",
-      image:
-        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
-    },
-    {
-      name: "Jim Jam",
-      birthday: "09/20/1999",
-      department: "Technology",
-      phone: "909-999-9999",
-      email: "jim@gmail.com",
-      image:
-        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
-    },
-  ];
+function EmployeeList({ employees }) {
   return (
     <Wrapper>
-      {data?.map((person) => (
+      {employees?.map((em) => (
         // <div onClick={() => setSelectedPunk(punk.token_id)}>
         <EmployeeCard
-          key={person.phone}
-          name={person.name}
-          birthday={person.birthday}
-          department={person.department}
-          phone={person.phone}
-          email={person.email}
-          image={person.image}
+          key={em.id}
+          name={em.name}
+          birthday={em.birthday}
+          department={em.department}
+          phone={em.phone}
+          email={em.email}
+          image={em.image}
         />
         // </div>
       ))}
