2e3c3e06e6575fdb40743be526ace00ec0b69a0c
Aly Baez <alybaez@Alys-MBP.lan>
2022-06-10 22:12:54 -0700

    adds styling for current month and onClick sets pin to month birthdays shown

--- src/components/Month.js
+++ src/components/Month.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import styled from "styled-components";
 
-function Month({ name, icon, active, setFilterStatus, number, setMonthName }) {
+function Month({
+  name,
+  icon,
+  active,
+  setFilterStatus,
+  number,
+  setMonthName,
+  monthName,
+}) {
+  const [color, setColor] = useState("");
+  const [weight, setWeight] = useState("");
+  const [fontSize, setFontSize] = useState("");
   const handleStatus = (number) => {
     setMonthName(name);
     setFilterStatus(number);
   };
 
+  useEffect(() => {
+    if (monthName === name) {
+      setColor("red");
+      setWeight("900");
+      setFontSize("30px");
+    }
+  }, []);
+
   return (
     <Wrapper>
       <Main onClick={() => handleStatus(number)}>
         <Image background={icon} backgroundActive={active}></Image>
-        <Title>{name}</Title>
+        <Title style={{ color, fontWeight: weight, fontSize }}>{name}</Title>
+        {monthName === name ? (
+          <img
+            style={{ height: "26px", marginLeft: "12px" }}
+            src="https://img.icons8.com/ios-filled/50/undefined/pin3.png"
+            alt="Active Month"
+          />
+        ) : (
+          <span></span>
+        )}
       </Main>
     </Wrapper>
   );
@@ -31,6 +59,7 @@ const Main = styled.div`
     background-color: lightyellow;
     border-radius: 30px;
     color: grey;
+    width: fit-content;
     transition: color 100ms ease-out;
   }
 `;
