55df4c095f6350bc7cad63a0f86a57b4df744554
Aly Baez <alybaez@Alys-MBP.lan>
2022-06-14 12:03:17 -0700

    checks all functionality

--- src/helpers/handleDelete.js
+++ src/helpers/handleDelete.js
@@ -2,11 +2,16 @@ import { deleteDoc, doc } from "firebase/firestore";
 import db from "../firebase";
 
 const handleDelete = (id, filteredEmployees, setFilteredEmployees) => {
-  filteredEmployees.forEach((em) => {
-    if (em.id === id) {
-      deleteDoc(doc(db, "employees", em.id));
-    }
-  });
+  try {
+    filteredEmployees.forEach((em) => {
+      if (em.id === id) {
+        deleteDoc(doc(db, "employees", em.id));
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
   setFilteredEmployees(filteredEmployees);
 };
 
